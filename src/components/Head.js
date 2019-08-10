b9d5fb6fc37d3eac18effb62562c491076804eec
yiran-t <940193266@qq.com>
2019-08-10 20:03:31 +0800

    解决冲突

---
+++ src/components/Head.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Head extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Head</h2>
+      </div>
+    );
+  }
+}
+
+export default Head;
